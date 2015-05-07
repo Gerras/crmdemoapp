63d026ecc5df75ba474458fe4af26eab5c85d05a
Gerras <k.brauen@gmail.com>
2015-05-07 14:39:56 -0600

    first commit

---
+++ CrmDemoApp/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System.Web.Mvc;
+using CrmDemoApp.Infrastructure;
+
+namespace CrmDemoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult ViewContacts()
+        {
+            var xrmServiceContext = new XrmServiceConnector().CreateXrmServiceConnector();
+            var contacts = xrmServiceContext.ContactSet;
+            return View(contacts);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ CrmDemoApp/Infrastructure/XrmServiceConnector.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.ServiceModel.Description;
+using Microsoft.Xrm.Client;
+using Xrm;
+
+namespace CrmDemoApp.Infrastructure
+{
+    public class XrmServiceConnector : IDisposable
+    {
+        private readonly string _userName;
+        private readonly string _password;
+        private XrmServiceContext _xrmServiceContext;
+
+        public XrmServiceConnector()
+        {
+            _userName = ConfigurationManager.AppSettings["userName"];
+            _password = ConfigurationManager.AppSettings["password"];
+            CreateCrmConnection();
+        }
+
+        private CrmConnection CreateCrmConnection()
+        {
+            var credentials = new ClientCredentials();
+            credentials.UserName.UserName = _userName;
+            credentials.UserName.Password = _password;
+            credentials.Windows.ClientCredential = CredentialCache.DefaultNetworkCredentials;
+            return new CrmConnection("Xrm") {ClientCredentials = credentials};
+        }
+
+        public XrmServiceContext CreateXrmServiceConnector()
+        {
+            return _xrmServiceContext ?? (_xrmServiceContext = new XrmServiceContext(CreateCrmConnection()));
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
+        //private ShipperServicesDb _context;
+
+        //protected ShipperServicesDb Context
+        //{
+        //    get { return _context ?? (_context = new ShipperServicesDb()); }
+        //}
+
+        //protected BaseService()
+        //{
+        //}
+
+        //public void Dispose()
+        //{
+        //    if (_context != null)
+        //    {
+        //        _context.Dispose();    
+        //    }
+            
+            
+        //}
